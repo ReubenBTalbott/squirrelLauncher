5bd62c924ff884feea5a4715924e1254990bd46b
Reuben Talbott <reuben@talbott.ws>
2020-04-16 17:05:29 -0400

    Added the HX711 library as a dependency

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,27 @@
 #include <Arduino.h>
+#include "HX711.h"
+
+// HX711 circuit wiring
+const int LOADCELL_DOUT_PIN = 2;
+const int LOADCELL_SCK_PIN = 3;
+
+HX711 scale;
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(57600);
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if (scale.is_ready()) {
+    long reading = scale.read();
+    Serial.print("HX711 reading: ");
+    Serial.println(reading);
+  } else {
+    Serial.println("HX711 not found.");
+  }
+
+  delay(1000);
+  
 }
